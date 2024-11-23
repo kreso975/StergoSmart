fac67b4a3d458c1bcddd26674d6c1495397d3d1d
kreso975 <kresimir.kokanovic@gmail.com>
2024-11-23 04:15:51 +0100

    cleaUP

---
+++ handleHttp.h
@@ -0,0 +1,7 @@
+/* ======================================================================
+Function: sendJSONheaderReply
+Input   : byte type , String message
+Output  :  
+Comments:
+Purpose : Display as an response Web Header with JSON reply */
+void sendJSONheaderReply( byte type, String message );
--- handleHttp.ino
+++ handleHttp.ino
@@ -252,13 +252,6 @@ void sendWebhook( char* localURL, String data )
   http.end();
 }
 
-/* ======================================================================
-Function: sendJSONheaderReply
-Purpose : Display as an response Web Header with JSON reply
-Input   : byte type , String message
-Output  :  
-Comments: 
-====================================================================== */
 void sendJSONheaderReply( byte type, String message )
 {
   String output;
